ff9b156db3914f2a7b307f12e7027bcff5bcaff5
watayuz <w-desu@i.softbank.jp>
2016-11-04 03:10:39 +0900

    add button callback

--- test.py
+++ test.py
@@ -1,17 +1,41 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
 from kivy.config import Config
 
-# window settings
-Config.set('graphics', 'width', '400')
-Config.set('graphics', 'height', '800')
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+from kivy.uix.gridlayout import GridLayout
+
+class RootWidget(GridLayout):
+
+    def buttonClicked(self, btn):
+        print('press button')
+
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(cols=1)
+
+        button = Button(text='push Button')
+        button.bind(on_press=self.buttonClicked)
+
+        self.add_widget(Label(text='text Label'))
+        self.add_widget(button)
+
+
+
 
 class TestApp(App):
     def build(self):
-        self.title = 'test'
-        # self.icon = 'FILENAME'
-        return Label(text='hello world')
+
+        # window settings
+        Config.set('graphics', 'width', '300')
+        Config.set('graphics', 'height', '800')
+
+        self.root = RootWidget()
+
+        self.title = 'test title'
+        # self.icon = 'FILE NAME'
+        return self.root
 
 if __name__ == '__main__':
     TestApp().run()
