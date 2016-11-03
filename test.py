e31a45b364f3b9fe8c7830c3b71455575882d0fa
watayuz <w-desu@i.softbank.jp>
2016-11-04 00:10:45 +0900

    add config

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.config import Config
+
+# window settings
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '800')
 
 class TestApp(App):
     def build(self):
+        self.title = 'test'
+        # self.icon = 'FILENAME'
         return Label(text='hello world')
 
-TestApp().run()
+if __name__ == '__main__':
+    TestApp().run()
